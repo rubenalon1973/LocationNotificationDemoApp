e49cdcdc93c10de11480c0fc34463a6830bc77c2
rubenalon1973 <rubenalonso1973@gmail.com>
2023-07-14 17:42:39 +0200

    Initial Commit

---
+++ LocationNotificationDemoApp/LocationNotificationDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationNotificationDemoAppApp.swift
+//  LocationNotificationDemoApp
+//
+//  Created by Ruben Alonso on 14/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LocationNotificationDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
